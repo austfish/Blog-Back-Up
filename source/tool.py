1aae3ddfb81a20493ac8693b3d243fc4d563aa1b
yujingtao <yu_jingtao@venustech.com.cn>
2019-10-08 14:56:42 +0800

    add photos

--- source/tool.py
+++ source/tool.py
@@ -135,8 +135,8 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../../myblog/source/photos/data.json","w", 'utf-8') as fp:
-        json.dump(final_dict, fp)
+    with open("../../myblog/source/photos/data.json","w",encoding='utf-8') as fp:
+        json.dump(final_dict, fp, ensure_ascii=False)
 
 def cut_photo():
     """裁剪算法
