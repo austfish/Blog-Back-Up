c78219d466c026bcb1781380fecdb506ee4c2acf
yujingtao <yu_jingtao@venustech.com.cn>
2019-10-08 11:06:41 +0800

    add photos

--- source/tool.py
+++ source/tool.py
@@ -1,6 +1,4 @@
 #coding: utf-8
-# -*- coding:utf-8 -*-
-#coding: unicode_escape
 from PIL import Image
 import os
 import sys
@@ -133,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("H:/Blog/myblog/source/photos/data.json","w") as fp:
+    with open("../austfish.github.io/source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
@@ -148,7 +146,7 @@ def cut_photo():
             make_directory(src_dir)
         # business logic
         file_list = list_img_file(src_dir)
-        print file_list
+        # print file_list
         if file_list:
             print_help()
             for infile in file_list:
@@ -172,19 +170,12 @@ def git_operation():
     os.system('git commit -m "add photos"')
     os.system('git push origin master')
 
-def updata_blog():
-	"""
-	执行博客更新脚本
-	"""
-	print("开始更新博客")
-	os.system('cd ../../myblog && ./deploy')
-	
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
     git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
-    updata_blog()	   # 调用myblog文件夹中deploy代码实现自动更新博客数据
+    
     
     
     
